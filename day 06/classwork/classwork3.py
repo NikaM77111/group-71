bfff9c3405f2b27c994099c30e391d9d56c31fa6
NikaM77111 <nikamachavariani771@gmail.com>
2025-05-04 18:13:18 +0200

    second

---
+++ day 06/classwork/classwork3.py
@@ -0,0 +1,19 @@
+#first_user = "Lika"
+#2nduser = "Giorgi"
+#print(first user)     ----> wrong code
+
+first_user = "Lika"
+second_user = "Giorgi"
+print(first_user)     #  ------> corrected
+
+
+#first name = Data
+#last % name = "random"   ----> wrong code
+
+first_name = "Data"
+last_name = "random"  # ------> corrected
+
+
+
+
+
---
+++ day 06/classwork/classwork4.py
@@ -0,0 +1,10 @@
+#Num1 = 20
+#num2 = 30
+#print(num1 + num2)
+#kოდი გამოიტანს შეცდომას, რადგან Num1 და num1 არ არის ერთიდაიგივე, პითონი განასხვავებს დიდ და პატარა ასოებს.
+#მაგრამ მოცემულში print(num1 + num2) არ შეესაბამება არც Num1 და არც num2, ამიტომ იქნება შეცდომა NameError: name 'num1' is not defined
+
+#corrected
+num1 = 20
+num2 = 30
+print(num1 + num2)

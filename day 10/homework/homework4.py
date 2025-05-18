2715611c4196a9ba39329ff5115993217dfca068
NikaM77111 <nikamachavariani771@gmail.com>
2025-05-18 15:12:14 +0200

    second

--- day 10/homework/homework4.py
+++ day 10/homework/homework4.py
@@ -3,5 +3,5 @@ surname = "ბერიძე"
 age = 21
 
 # გადავიყვანოთ age სტრინგად, რათა თავიდან ავიცილოთ შეცდომა კონკატინაციისას
-sentence = "მე ვარ " + name + " " + surname + ", ჩემი ასაკია " + str(age) + " წელი."
+sentence = "მე ვარ" + name + " " + surname + ", ჩემი ასაკია " + str(age) + " წელი."
 print(sentence)
